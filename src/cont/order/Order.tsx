7ed6b21e20a7c24ef5ad8a2292fa585db3817a65
ㅇㅅㅇ <rlawlsrb4011@naver.com>
2026-07-25 09:42:24 +0900

    dtd브랜치에서 관리하는 모든 컴포넌트 css모듈 최적화 했습니다(디자인서식 통일) / 프로덕트컴포넌트에서 인풋태그에 값을 입력하고 이벤트 핸들러를 동작했을때 인풋태그의 값이 초기화되지 않는 오류가 수정되었습니다

--- src/cont/order/Order.tsx
+++ src/cont/order/Order.tsx
@@ -5,7 +5,6 @@ import axios from 'axios';
 
 
 
-//발주테이블, 서명을 이미지로 받을 컬럼 수정해야함 지금 없음
 export interface OrderForm {
     ofnum?: number;
     mnum: string;
@@ -22,6 +21,7 @@ interface OrderItem {
     oiname: string;
     oiprice: number;
     oiamount: number;
+    oiSumPrice: number;
     oipublisher: string;
 }
 
@@ -44,16 +44,21 @@ const Order: React.FC = () => {
     const [oipublisher, setOipublisher] = useState<string>('');
     const [oiprice, setOiprice] = useState<number>(0);
     const [oiamount, setOiamount] = useState<number>(0);
-
+    
     const itmeList = () => {
+
+        let oiSumPrice = oiprice*oiamount;
+
         const inputRowList = {
             oiname: oiname,
             oiprice: oiprice,
             oipublisher: oipublisher,
-            oiamount: oiamount
+            oiamount: oiamount,
+            oiSumPrice: oiSumPrice
         }
         setOrderItem([...orderItem, inputRowList]);
 
+        oiSumPrice = 0;
         setOiname('');
         setOipublisher('');
         setOiprice(0);
@@ -90,65 +95,147 @@ const Order: React.FC = () => {
 
     console.log(orderForm);
     return (
-        <div>
-            <div className={Stayle.header_container}>
-                <h2 className={Stayle.header_container_text_right}>발주자 작성란</h2>
-                <h3 className={Stayle.div_text}>서명 및 발주하기</h3>
-            </div>
-            <form>
-                <div className={Stayle.header_container}>
-                    <div className={Stayle.header_container_text_right}>
-                        <ul className={Stayle.header_container_li}>
-                            <li>대표자 : <input type="text" name='oname' onChange={(e) => setOname(e.target.value)} /></li>
-                            <li>주소 : <input type="text" name='oaddr' onChange={(e) => setOaddr(e.target.value)} /></li>
-                            <li>법인명 : <input type="text" name='ofcompany' onChange={(e) => setOfcompany(e.target.value)} /></li>
-                            <li>연락처 : <input type="text" name='ophone' onChange={(e) => setOphone(e.target.value)} /></li>
-                            <li>발주일 : <input type="date" name='ofdate' onChange={(e) => setOfdate(e.target.value)} /></li>
-                        </ul>
-                    </div>
-                    <div className={Stayle.header_container_text_left}>
-                        {/* <ul>
-            <li>발주일 : <input type="text" name='date' /></li>
-            <li>발주일 : <input type="text" name='date' /></li>
-            <li>발주일 : <input type="text" name='date' /></li>
-            <li>발주일 : <input type="text" name='date' /></li>
-            <li>발주일 : <input type="text" name='date' /></li>
-          </ul> */}
+        <div className={Stayle.container}>
 
-                        <Signature order={orderForm} />
-                        {/* <input type="button" name='date'>발주하기</input> */}
-                    </div>
+            <div className={Stayle.header_title_area}>
+                <h2>발주자 작성란</h2>
+                <h3>서명 및 발주하기</h3>
+            </div>
+
+            <div className={Stayle.header_container}>
+                <div className={Stayle.header_container_text_right}>
+                    <ul className={Stayle.header_container_li}>
+                        <li>
+                            대표자 :{' '}
+                            <input
+                                type="text"
+                                name="oname"
+                                value={oname}
+                                onChange={(e) => setOname(e.target.value)}
+                            />
+                        </li>
+                        <li>
+                            주소 :{' '}
+                            <input
+                                type="text"
+                                name="oaddr"
+                                value={oaddr}
+                                onChange={(e) => setOaddr(e.target.value)}
+                            />
+                        </li>
+                        <li>
+                            법인명 :{' '}
+                            <input
+                                type="text"
+                                name="ofcompany"
+                                value={ofcompany}
+                                onChange={(e) => setOfcompany(e.target.value)}
+                            />
+                        </li>
+                        <li>
+                            연락처 :{' '}
+                            <input
+                                type="text"
+                                name="ophone"
+                                value={ophone}
+                                onChange={(e) => setOphone(e.target.value)}
+                            />
+                        </li>
+                        <li>
+                            발주일 :{' '}
+                            <input
+                                type="date"
+                                name="ofdate"
+                                value={ofdate}
+                                onChange={(e) => setOfdate(e.target.value)}
+                            />
+                        </li>
+                    </ul>
                 </div>
-                <table >
-                    <thead>
-                        <tr>
-                            <th>도서명</th>
-                            <th>출판사</th>
-                            <th>단가</th>
-                            <th>합계금액</th>
-                            <th>수량</th>
+                <div className={Stayle.header_container_text_left}>
+                    <Signature order={orderForm} />
+                </div>
+            </div>
+
+            <table className={Stayle.Table}>
+                <thead>
+                    <tr>
+                        <th>도서명</th>
+                        <th>출판사</th>
+                        <th>단가</th>
+                        <th>합계금액</th>
+                        <th>수량</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <input
+                                type="text"
+                                name="oiname"
+                                value={oiname}
+                                onChange={(e) => setOiname(e.target.value)}
+                            />
+                        </td>
+                        <td>
+                            <input
+                                type="text"
+                                name="oipublisher"
+                                value={oipublisher}
+                                onChange={(e) => setOipublisher(e.target.value)}
+                            />
+                        </td>
+                        <td>
+                            <input
+                                type="number"
+                                name="oiprice"
+                                value={oiprice || ''}
+                                onChange={(e) => setOiprice(parseInt(e.target.value) || 0)}
+                            />
+                        </td>
+                        <td>{(oiprice * oiamount).toLocaleString()} 원</td>
+                        <td>
+                            <input
+                                type="number"
+                                name="oiamount"
+                                value={oiamount || ''}
+                                onChange={(e) => setOiamount(parseInt(e.target.value) || 0)}
+                            />
+                        </td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan={5}>
+                            <button type="button" onClick={itmeList}>
+                                주문리스트 추가!
+                            </button>
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+            <table className={Stayle.Table}>
+                <thead>
+                    <tr>
+                        <th>도서명</th>
+                        <th>출판사</th>
+                        <th>단가</th>
+                        <th>합계금액</th>
+                        <th>수량</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {orderItem.map((e, i) => (
+                        <tr key={i}>
+                            <td>{e.oiname}</td>
+                            <td>{e.oipublisher}</td>
+                            <td>{e.oiprice}</td>
+                            <td>{e.oiSumPrice}</td>
+                            <td>{e.oiamount}</td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th><input type="text" name='oiname' onChange={(e) => setOiname(e.target.value)} /></th>
-                            <th><input type="text" name='oipublisher' onChange={(e) => setOipublisher(e.target.value)} /></th>
-                            <th><input type="number" name='oiprice' onChange={(e) => setOiprice(Number(e.target.value))} /></th>
-                            <th>{oiprice * oiamount} 원</th>
-                            <th><input type="number" name='oiamount' onChange={(e) => setOiamount(Number(e.target.value))} /></th>
-                        </tr>
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <th colSpan={5}>
-                                <button type="button" onClick={itmeList}>
-                                    주문리스트 추가!
-                                </button>
-                            </th>
-                        </tr>
-                    </tfoot>
-                </table>
-            </form>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
